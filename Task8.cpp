4742da8a1cccb7eafec31b1fe8e4584f4c9f7e31
Abubakar <52706546+Abubakar0111@users.noreply.github.com>
2021-03-06 12:18:19 +0500

    initial commit

---
+++ Task8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+     int count ;
+     cout<<"Enter Count : ";
+     cin>>count;
+    cout<<++count<<endl;
+    cout<<--count<<endl;
+    cout<<count++<<endl;
+    cout<<count--<<endl;
+
+
+}
